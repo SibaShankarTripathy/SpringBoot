7287ab279677426455abb7ad41e4a4709b50a982
SibaShankar <sibashankartripathy4@gmail.com>
2024-06-16 18:01:27 +0530

    UseJPARepository

---
+++ SB_DataJPA_P4_UseJPARepository/src/main/java/com/sst/SB_DataJPA_P4_UseJPARepository.java
@@ -0,0 +1,13 @@
+package com.sst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SB_DataJPA_P4_UseJPARepository {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SB_DataJPA_P4_UseJPARepository.class, args);
+	}
+
+}
---
+++ SB_DataJPA_P4_UseJPARepository/src/test/java/com/sst/BootDataJpaProj04sortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDataJpaProj04sortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
